7cfe342b3870816ab6dd1ae4b2a6ddc906491500
Siddhant <shaurya.sid1729@gmail.com>
2024-11-05 11:52:24 +0530

    Add files via upload

---
+++ project.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    printf("***************************************\n");
+printf("**********************************\n");
+printf("*********************\n");
+printf("***********Name of the system**********\n");
+printf("***************************************\n");
+printf("***************************************\n");
+char correctPassword[] = "pass$123"; 
+printf("enter your username");
+    return 0;
+}
