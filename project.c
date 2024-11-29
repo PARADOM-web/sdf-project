41717ac9ea744f36fb4fea762cc8a4db6e6a0c71
PARADOM-web <himanshugujar2006@gmail.com>
2024-11-30 01:12:26 +0530

    Update project.c

--- project.c
+++ project.c
@@ -1,13 +1,18 @@
-#include<stdio.h>
-
-int main(){
-    printf("***************************************\n");
-printf("**********************************\n");
-printf("*********************\n");
-printf("***********Name of the system**********\n");
-printf("***************************************\n");
-printf("***************************************\n");
-char correctPassword[] = "pass$123"; 
-printf("enter your username");
-    return 0;
+#include <stdio.h>
+void main(){
+    printf("                       ## # # # # # # # # # # # # # # # # # ##\n");
+    printf( "                     ##                                   ##\n");
+    printf(  "                   ##                                   ##\n");
+    printf("                 ##                                   ##\n");
+    printf("               ##                                   ##\n" );
+    printf("             ##                                   ##\n");
+    printf("           ##                                   ##\n");
+    printf("         ##  # # # # # # # # # # # # # # # #  ##\n");
+    printf( "          ##                                   ##\n");
+    printf(  "           ##      #         #         #        ##\n");
+    printf(   "            ##                                   ##\n");
+    printf(    "             ##      #         #          #       ##\n");
+    printf(     "              ##                                   ##\n");
+    printf(      "               ##      #         #          #       ##\n");
+    printf(       "                ## # # # # # # # # # # # # # # # # # ##\n");
 }
